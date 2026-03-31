a6d1e411b96efcef8428d6482efc73d20699943c
Pranay B <b.pranay.git@gmail.com>
2026-03-31 14:53:53 +0530

    revert api routes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -30,33 +30,33 @@ api.interceptors.request.use(
 // ------------------- LOCATION APIs -------------------
 
 export const fetchLocations = (params) =>
-  api.get('/api/locations', { params }).then((r) => r.data);
+  api.get('/locations', { params }).then((r) => r.data);
 
 export const fetchLocationById = (id, lang = 'en') =>
-  api.get(`/api/locations/${id}`, { params: { lang } }).then((r) => r.data);
+  api.get(`/locations/${id}`, { params: { lang } }).then((r) => r.data);
 
 export const searchLocations = (q, lang = 'en') =>
-  api.get('/api/locations/search', { params: { q, lang } }).then((r) => r.data);
+  api.get('/locations/search', { params: { q, lang } }).then((r) => r.data);
 
 export const fetchNearby = (id, radius = 10000, category = '', lang = 'en') =>
   api
-    .get(`/api/locations/${id}/nearby`, {
+    .get(`/locations/${id}/nearby`, {
       params: { radius, category, lang },
     })
     .then((r) => r.data);
 
 export const fetchReviews = (id) =>
-  api.get(`/api/locations/${id}/reviews`).then((r) => r.data);
+  api.get(`/locations/${id}/reviews`).then((r) => r.data);
 
 export const postReview = (id, data) =>
-  api.post(`/api/locations/${id}/reviews`, data).then((r) => r.data);
+  api.post(`/locations/${id}/reviews`, data).then((r) => r.data);
 
 // ------------------- OTHER APIs -------------------
 
 export const fetchGuides = (city) =>
-  api.get('/api/guides', { params: { city } }).then((r) => r.data);
+  api.get('/guides', { params: { city } }).then((r) => r.data);
 
 export const fetchCategories = () =>
-  api.get('/api/categories').then((r) => r.data);
+  api.get('/categories').then((r) => r.data);
 
 export default api;
