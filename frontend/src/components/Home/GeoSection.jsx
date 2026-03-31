f1dc8576481dbb3e335b3aebd23f12dfbb724cf2
Pranay B <b.pranay.git@gmail.com>
2026-03-31 16:15:33 +0530

    revert geosection

--- frontend/src/components/Home/GeoSection.jsx
+++ frontend/src/components/Home/GeoSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from 'axios'; //reverted
 import { useApp } from '../../store/AppContext';
 
 const GeoSection = () => {
